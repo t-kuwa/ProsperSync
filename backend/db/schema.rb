1eafd740920678b424473285827b4f33773d2d7f
Tomoya Kuwashima <68315619+kuwa-9710@users.noreply.github.com>
2025-11-02 07:37:57 +0900

    fix: Remove duplicate created_at and updated_at fields in users table schema

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -59,12 +59,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_01_090600) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", comment: "ユーザー", force: :cascade do |t|
     t.string "name", null: false, comment: "名前"
     t.string "email", null: false, comment: "メールアドレス"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
